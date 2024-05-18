6530f7aa538b34c7805c764a55d38fa541462596
ahmmadSumon <arifahmmadsumon@gmail.com>
2024-05-18 12:18:48 +0600

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -21,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar/>
         <main>
+        <SpeedInsights />
         {children}
         </main>
         <Footer/>
