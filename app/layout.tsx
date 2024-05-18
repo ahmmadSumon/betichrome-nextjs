f5f2873e7a7e8d0d0783aca9aa94fd9e807f3442
ahmmadSumon <arifahmmadsumon@gmail.com>
2024-05-18 14:33:56 +0600

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
